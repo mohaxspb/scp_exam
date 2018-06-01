5f62ca87d8519376888322dc203231a82a920672
yuri <y.kuchanov@redmadrobot.com>
2018-06-01 20:10:29 +0300

    - constant updates of levels on its screen

--- app/src/main/java/ru/kuchanov/scpquiz/mvp/presenter/AppInfoPresenter.kt
+++ app/src/main/java/ru/kuchanov/scpquiz/mvp/presenter/AppInfoPresenter.kt
@@ -2,12 +2,22 @@ package ru.kuchanov.scpquiz.mvp.presenter
 
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
+import ru.kuchanov.scpquiz.controller.db.AppDatabase
+import ru.kuchanov.scpquiz.model.db.FinishedLevels
 import ru.kuchanov.scpquiz.mvp.view.AppInfoView
+import ru.terrakok.cicerone.Router
 import timber.log.Timber
 import javax.inject.Inject
 
 @InjectViewState
-class AppInfoPresenter @Inject constructor() : MvpPresenter<AppInfoView>() {
+class AppInfoPresenter @Inject constructor(
+    private var appDatabase: AppDatabase,
+    private var router: Router
+) : MvpPresenter<AppInfoView>() {
 
     init {
         Timber.d("constructor")
@@ -18,5 +28,16 @@ class AppInfoPresenter @Inject constructor() : MvpPresenter<AppInfoView>() {
         super.onDestroy()
     }
 
-    fun onSomethingClick() = viewState.showMessage("onSomethingClick from Fragment")
+    fun onSomethingClick() {
+        viewState.showMessage("onSomethingClick from Fragment")
+        //fixme test
+        Single.fromCallable { appDatabase.finishedLevelsDao().insert(FinishedLevels(quizId = 6, finished = true)) }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                    onSuccess = {
+                        Timber.d("updated!")
+                    }
+                )
+    }
 }
--- app/src/main/java/ru/kuchanov/scpquiz/mvp/presenter/LevelsPresenter.kt
+++ app/src/main/java/ru/kuchanov/scpquiz/mvp/presenter/LevelsPresenter.kt
@@ -7,6 +7,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.functions.BiFunction
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
+import ru.kuchanov.scpquiz.Constants
 import ru.kuchanov.scpquiz.controller.adapter.viewmodel.LevelViewModel
 import ru.kuchanov.scpquiz.controller.db.AppDatabase
 import ru.kuchanov.scpquiz.model.db.FinishedLevels
@@ -14,7 +15,6 @@ import ru.kuchanov.scpquiz.model.db.Quiz
 import ru.kuchanov.scpquiz.mvp.view.LevelsView
 import ru.terrakok.cicerone.Router
 import timber.log.Timber
-import java.util.*
 import javax.inject.Inject
 
 @InjectViewState
@@ -30,18 +30,31 @@ class LevelsPresenter @Inject constructor(
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
 
-        appDatabase.quizDao().getAll()
-                //todo get completed level value from somewhere
-                .map {
-                    it.map { LevelViewModel(it, Random().nextBoolean()) }
-                }
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeBy(
-                    onNext = { viewState.showLevels(it) }
-                )
+        updateLevels()
+    }
 
-        Flowable.zip(
+    override fun onDestroy() {
+        Timber.d("onDestroy")
+        super.onDestroy()
+    }
+
+    fun onLevelClick(quizId: Long) {
+        Timber.d("onLevelClick: %s", quizId)
+        //todo navigate to game screen
+        router.navigateTo(Constants.Screens.QUIZ, quizId)
+
+//        Single.fromCallable { appDatabase.finishedLevelsDao().insert(FinishedLevels(quizId = quizId, finished = true)) }
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribeBy(
+//                    onSuccess = {
+//                        Timber.d("updated!")
+//                    }
+//                )
+    }
+
+    fun updateLevels() {
+        Flowable.combineLatest(
             appDatabase.quizDao().getAll(),
             appDatabase.finishedLevelsDao().getAll(),
             BiFunction { t1: List<Quiz>, t2: List<FinishedLevels> -> t1 to t2 }
@@ -55,26 +68,10 @@ class LevelsPresenter @Inject constructor(
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeBy(
-                    onNext = { viewState.showLevels(it) }
+                    onNext = {
+                        Timber.d("updateLevels onNext")
+                        viewState.showLevels(it)
+                    }
                 )
-    }
-
-    override fun onDestroy() {
-        Timber.d("onDestroy")
-        super.onDestroy()
-    }
-
-    fun onLevelClick(quizId: Long) {
-        Timber.d("onLevelClick: %s", quizId)
-        //todo navigate to game screen
-//        router.navigateTo(Constants.Screens.QUIZ, quizId)
-        //test
-        Flowable.fromCallable { appDatabase.finishedLevelsDao().insert(FinishedLevels(quizId = quizId, finished = true)) }
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeBy(
-                    onNext = { Timber.d("updated!") }
-                )
-
     }
 }
