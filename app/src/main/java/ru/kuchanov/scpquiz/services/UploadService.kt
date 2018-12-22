4af8a43f7275e326e7f66e0b868944b72ced50de
yuri <y.kuchanov@redmadrobot.com>
2018-12-22 17:37:50 +0300

    Fix missed FOREGROUND_SERVICE permission. Fix not set Chanel for notification from service on API>=26. Move notification ID to constants. Fix not destroying service if downloads failed to complete. Move notificationManager initialization to onCreate and make it class member.

--- app/src/main/java/ru/kuchanov/scpquiz/services/UploadService.kt
+++ app/src/main/java/ru/kuchanov/scpquiz/services/UploadService.kt
@@ -1,9 +1,12 @@
 package ru.kuchanov.scpquiz.services
 
 import android.app.Application
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.support.v4.app.NotificationCompat
 import io.reactivex.Maybe
@@ -18,11 +21,10 @@ import ru.kuchanov.scpquiz.controller.navigation.ScpRouter
 import ru.kuchanov.scpquiz.di.Di
 import ru.kuchanov.scpquiz.model.api.QuizConverter
 import ru.kuchanov.scpquiz.ui.activity.MainActivity
+import ru.kuchanov.scpquiz.utils.createNotificationChannel
 import timber.log.Timber
 import toothpick.Toothpick
 import javax.inject.Inject
-import android.app.NotificationManager
-import android.content.Context
 
 
 class UploadService : Service() {
@@ -40,6 +42,8 @@ class UploadService : Service() {
     @Inject
     lateinit var quizConverter: QuizConverter
 
+    lateinit var notificationManager: NotificationManager
+
     override fun onBind(intent: Intent): IBinder {
         throw UnsupportedOperationException("Not yet implemented")
     }
@@ -47,19 +51,22 @@ class UploadService : Service() {
     override fun onCreate() {
         super.onCreate()
         Toothpick.inject(this, Toothpick.openScope(Di.Scope.APP))
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
         val notificationIntent = Intent(this, MainActivity::class.java)
 
         val pendingIntent = PendingIntent.getActivity(this, 0,
                 notificationIntent, 0)
 
-        val notification = NotificationCompat.Builder(this)
+        val notification = NotificationCompat.Builder(this, getChanelId())
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Scp quiz")
                 .setContentText("Downloading data")
                 .setContentIntent(pendingIntent)
                 .build()
 
-        startForeground(1337, notification)
+        startForeground(NOTIFICATION_ID, notification)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -78,14 +85,42 @@ class UploadService : Service() {
                 .subscribeOn(Schedulers.io())
                 .observeOn(mainThread())
                 .subscribeBy(
-                        onError = { error: Throwable -> Timber.e(error) },
+                        onError = { error: Throwable ->
+                            Timber.e(error)
+                            stopServiceAndRemoveNotification()
+                        },
                         onComplete = {
                             Timber.d("onComplete SERVICE")
-                            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-                            notificationManager.cancel(1337)
-                            stopForeground(true)
+                            stopServiceAndRemoveNotification()
                         }
                 )
         return Service.START_NOT_STICKY
+    }
+
+    private fun stopServiceAndRemoveNotification() {
+        Timber.d("stopServiceAndRemoveNotification")
+        notificationManager.cancel(NOTIFICATION_ID)
+        stopForeground(true)
+        stopSelf()
+    }
+
+    private fun getChanelId(): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(
+                    this,
+                    CHANEL_ID,
+                    CHANEL_NAME
+            )
+        } else {
+            ""
+        }
+    }
+
+    companion object {
+        const val NOTIFICATION_ID = 1337
+
+        const val CHANEL_ID = "DOWNLOADS_CHANEL_ID"
+
+        const val CHANEL_NAME = "DOWNLOADS_CHANEL_NAME"
     }
 }
