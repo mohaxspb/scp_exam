b57bb7435c28c04853b0a0fb3a643dd7e3cf02d9
yuri <y.kuchanov@redmadrobot.com>
2019-01-08 23:35:33 +0300

    Add migration with setting authorId be nullable for Quiz, Translation and Phrase. Set authorId be nullable for NwQuiz, NwTranslation and NwPhrase.

--- app/src/main/java/ru/kuchanov/scpquiz/controller/db/migrations/Migrations.kt
+++ app/src/main/java/ru/kuchanov/scpquiz/controller/db/migrations/Migrations.kt
@@ -52,17 +52,84 @@ object Migrations {
 
     val MIGRATION_3_4 = object : Migration(3, 4) {
         override fun migrate(database: SupportSQLiteDatabase) {
-            Timber.d("executeMigration 2_3")
-            //add column for available property with true as default
+            Timber.d("executeMigration 3_4")
             try {
+                //set quiz.authorId be nullable
                 database.execSQL(
-                        //todo quiz, translation, phrase. Get sql from /schemas
                         """
-                            CREATE TABLE Quiz(..., JOB_TYPES NULL, ...);
-                            INSERT INTO Jobs2 SELECT * FROM Jobs;
-                            DROP TABLE Jobs;
-                            ALTER TABLE Jobs2 RENAME TO Jobs;
+                            CREATE TABLE QuizTemp(
+                                id INTEGER NOT NULL,
+                                scpNumber TEXT NOT NULL,
+                                imageUrl TEXT NOT NULL,
+                                authorId INTEGER,
+                                approved INTEGER NOT NULL,
+                                approverId INTEGER,
+                                created INTEGER NOT NULL,
+                                updated INTEGER NOT NULL,
+                                PRIMARY KEY(id)
+                            )
                             """)
+                database.execSQL("""
+                    INSERT INTO QuizTemp SELECT * FROM Quiz
+                    """)
+                database.execSQL("""
+                    DROP TABLE Quiz;
+                    """)
+                database.execSQL("""
+                    ALTER TABLE QuizTemp RENAME TO Quiz;
+                    """)
+
+                //set quizTranslation.authorId be nullable
+                database.execSQL(
+                        """
+                            CREATE TABLE QuizTranslationTemp(
+                                id INTEGER NOT NULL,
+                                quizId INTEGER NOT NULL,
+                                langCode TEXT NOT NULL,
+                                translation TEXT NOT NULL,
+                                description TEXT NOT NULL,
+                                approved INTEGER NOT NULL,
+                                authorId INTEGER,
+                                approverId INTEGER,
+                                created INTEGER NOT NULL,
+                                updated INTEGER NOT NULL,
+                                PRIMARY KEY(id)
+                            )
+                            """)
+                database.execSQL("""
+                    INSERT INTO QuizTranslationTemp SELECT * FROM QuizTranslation
+                    """)
+                database.execSQL("""
+                    DROP TABLE QuizTranslation;
+                    """)
+                database.execSQL("""
+                    ALTER TABLE QuizTranslationTemp RENAME TO QuizTranslation;
+                    """)
+
+                //set quizTranslationPhrase.authorId be nullable
+                database.execSQL(
+                        """
+                            CREATE TABLE QuizTranslationPhraseTemp(
+                                id INTEGER NOT NULL,
+                                quizTranslationId INTEGER NOT NULL,
+                                translation TEXT NOT NULL,
+                                approved INTEGER NOT NULL,
+                                authorId INTEGER,
+                                approverId INTEGER,
+                                created INTEGER NOT NULL,
+                                updated INTEGER NOT NULL,
+                                PRIMARY KEY(id)
+                            )
+                            """)
+                database.execSQL("""
+                    INSERT INTO QuizTranslationPhraseTemp SELECT * FROM QuizTranslationPhrase
+                    """)
+                database.execSQL("""
+                    DROP TABLE QuizTranslationPhrase;
+                    """)
+                database.execSQL("""
+                    ALTER TABLE QuizTranslationPhraseTemp RENAME TO QuizTranslationPhrase;
+                    """)
             } catch (e: Throwable) {
                 Timber.e(e)
             }
